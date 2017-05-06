ff9bcb1abd78f53e83a7943e229bd7c74e7f57fa
Prashanthi Kadambi <Prashanthi.S.K@iiitb.org>
2017-05-06 19:14:35 +0530

    Add files via upload

--- dhtard/dhtard.ino
+++ dhtard/dhtard.ino
@@ -3,16 +3,17 @@
 #include <Wire.h>
 #include "RTClib.h"
 #include <NewPing.h>
+#include <SparkFun_ADXL345.h>      
 
-#define TRIGGER_PIN  12
-#define ECHO_PIN     11
+#define TRIGGER_PIN  8
+#define ECHO_PIN     4
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
 #include <SoftwareSerial.h>
 SoftwareSerial BTSerial(6,7); // RX | TX
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 RTC_DS3231 rtc;
-
+ADXL345 adxl = ADXL345();  
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 #define ALTITUDE 920.0 // Altitude of SparkFun's HQ in Boulder, CO. in meters
@@ -28,8 +29,9 @@ dht DHT;
 char status;
 double T,P,p0,a;
 int calibrationTime = 30;        
-int ledPin = 13;
+int ledPin = 9;
 int pirPin=3;
+int accPin=13;
 //the time when the sensor outputs a low impulse
 long unsigned int lowIn;         
 
@@ -38,6 +40,8 @@ long unsigned int lowIn;
 long unsigned int pause = 5000;  
 
 boolean lockLow = true;
+boolean var=false;
+boolean mvt=true;
 boolean takeLowTime; 
 String Date; 
 // You will need to create an SFE_BMP180 object, here called "pressure":
@@ -46,9 +50,11 @@ SFE_BMP180 pressure;
 
 void setup(){
   Serial.begin(38400);
-  BTSerial.begin(38400);  
+  BTSerial.begin(38400); 
+     Serial.println("Welcome to the Warehouse monitoring system\n");
+   Serial.println("Setting up all the sensors");
   delay(300);//Let system settle
-  Serial.println("Humidity and temperature\n\n");
+  Serial.println("Humidity and temperature");
   delay(700);
     if (pressure.begin())
     Serial.println("BMP180 init success");
@@ -59,6 +65,48 @@ void setup(){
 
     Serial.println("BMP180 init fail\n\n");
     while(1); // Pause forever.
+     adxl.powerOn();                     // Power on the ADXL345
+
+  adxl.setRangeSetting(16);           // Give the range settings
+                                      // Accepted values are 2g, 4g, 8g or 16g
+                                      // Higher Values = Wider Measurement Range
+                                      // Lower Values = Greater Sensitivity
+
+  adxl.setSpiBit(0);                  // Configure the device to be in 4 wire SPI mode when set to '0' or 3 wire SPI mode when set to 1
+                                      // Default: Set to 1
+                                      // SPI pins on the ATMega328: 11, 12 and 13 as reference in SPI Library 
+   
+  adxl.setActivityXYZ(1, 0, 0);       // Set to activate movement detection in the axes "adxl.setActivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+  adxl.setActivityThreshold(75);      // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
+ 
+  adxl.setInactivityXYZ(1, 0, 0);     // Set to detect inactivity in all the axes "adxl.setInactivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+  adxl.setInactivityThreshold(75);    // 62.5mg per increment   // Set inactivity // Inactivity thresholds (0-255)
+  adxl.setTimeInactivity(10);         // How many seconds of no activity is inactive?3
+ 
+  // Set values for what is considered a TAP and what is a DOUBLE TAP (0-255)
+  adxl.setTapThreshold(50);           // 62.5 mg per increment
+  adxl.setTapDuration(15);            // 625 μs per increment
+  adxl.setDoubleTapLatency(80);       // 1.25 ms per increment
+  adxl.setDoubleTapWindow(200);       // 1.25 ms per increment
+ 
+  // Set values for what is considered FREE FALL (0-255)
+  adxl.setFreeFallThreshold(7);       // (5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(30);       // (20 - 70) recommended - 5ms per increment
+ 
+  // Setting all interupts to take place on INT1 pin
+  //adxl.setImportantInterruptMapping(1, 1, 1, 1, 1);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);" 
+                                                        // Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
+                                                        // This library may have a problem using INT2 pin. Default to INT1 pin.
+  
+  // Turn on Interrupts for each mode (1 == ON, 0 == OFF)
+  adxl.InactivityINT(1);
+  adxl.ActivityINT(1);
+  adxl.FreeFallINT(1);
+  adxl.doubleTapINT(1);
+  adxl.singleTapINT(1);
+  
+//attachInterrupt(digitalPinToInterrupt(interruptPin), ADXL_ISR, RISING);   // Attach Interrupt
+
   }
 
   
@@ -80,6 +128,7 @@ void setup(){
   }
    pinMode(pirPin, INPUT);
    pinMode(ledPin, OUTPUT);
+   pinMode(accPin, OUTPUT);
    digitalWrite(pirPin, LOW);
 
   //give the sensor some time to calibrate
@@ -91,7 +140,7 @@ void setup(){
     Serial.println(" done");
     Serial.println(" PIR SENSOR ACTIVE");
     delay(50);
-  
+  Serial.println("setup done.beginning transmission of sensor data");
 
 }
 
@@ -171,6 +220,7 @@ void loop(){
          lockLow = false;            
          Serial.println("---");
          Serial.print("motion detected at ");
+         var=true;
          Serial.print(millis()/1000);
          Serial.println(" sec"); 
          delay(50);
@@ -179,7 +229,7 @@ void loop(){
        }
 
      if(digitalRead(pirPin) == LOW){       
-       digitalWrite(ledPin, LOW);  //the led visualizes the sensors output pin state
+       //the led visualizes the sensors output pin state
 
        if(takeLowTime){
         lowIn = millis();          //save the time of the transition from high to LOW
@@ -193,12 +243,66 @@ void loop(){
            lockLow = true;                        
            Serial.print("motion ended at ");      //output
            Serial.print((millis() - pause)/1000);
+            digitalWrite(ledPin, LOW); 
+            var=false;
            Serial.println(" sec");
            delay(50);
            }
        }
+       BTSerial.println(var);
+       int x,y,z;   
+  adxl.readAccel(&x, &y, &z);         // Read the accelerometer values and store them in variables declared above x,y,z
+
+  // Output Results to Serial
+  /* UNCOMMENT TO VIEW X Y Z ACCELEROMETER VALUES */  
+  /*Serial.print(x);
+  Serial.print(", ");
+  Serial.print(y);
+  Serial.print(", ");
+  Serial.println(z); 
+  */
+  ADXL_ISR();
+  BTSerial.println(mvt);
 
 Serial.println();
 Serial.println();
 }
-
+void ADXL_ISR() {
+  
+  // getInterruptSource clears all triggered actions after returning value
+  // Do not call again until you need to recheck for triggered actions
+  byte interrupts = adxl.getInterruptSource();
+  
+  // Free Fall Detection
+  if(adxl.triggered(interrupts, ADXL345_FREE_FALL)){
+    Serial.println("*** FREE FALL ***");
+    //add code here to do when free fall is sensed
+  } 
+  
+  // Inactivity
+  if(adxl.triggered(interrupts, ADXL345_INACTIVITY)){
+    Serial.println("*** INACTIVITY ***");
+     //add code here to do when inactivity is sensed
+     mvt=false;
+     digitalWrite(accPin,LOW);
+  }
+  
+  // Activity
+  if(adxl.triggered(interrupts, ADXL345_ACTIVITY)){
+    Serial.println("*** ACTIVITY ***"); 
+     //add code here to do when activity is sensed
+      digitalWrite(accPin,HIGH);
+  }
+  
+  // Double Tap Detection
+  if(adxl.triggered(interrupts, ADXL345_DOUBLE_TAP)){
+    Serial.println("*** DOUBLE TAP ***");
+     //add code here to do when a 2X tap is sensed
+  }
+  
+  // Tap Detection
+  if(adxl.triggered(interrupts, ADXL345_SINGLE_TAP)){
+    Serial.println("*** TAP ***");
+     //add code here to do when a tap is sensed
+  } 
+}
